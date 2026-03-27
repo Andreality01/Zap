d6852612d1aef29cfeb56d4e09ca9e7ea87ad16a
Luminyx <27lumi@protonmail.com>
2026-03-26 22:28:13 -0400

    Stingby: Major improvements

---
+++ include/zap/actor/Stingby.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <enemy/Enemy.h>
+#include <enemy/EnemyEatData.h>
+#include <enemy/EnemyChibiYoshiEatData.h>
+#include <graphics/JointBlendModel.h>
+
+namespace zap {
+
+class Stingby : public Enemy {
+    SEAD_RTTI_OVERRIDE(Stingby, Enemy)
+
+public:
+    static Profile* cProfile;
+
+    Stingby(const ActorCreateParam& param);
+    ~Stingby() override = default;
+
+    Result create() override;
+    bool execute() override;
+    bool draw() override;
+
+    bool createIceActor() override;
+    void setIceAnm() override;
+    
+    void vsPlayerHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    void vsYoshiHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    
+    void updateModel();
+
+    static ActorCollisionCheck::CollisionData cCollisionData;
+
+    DECLARE_STATE_ID(Stingby, Idle)
+    DECLARE_STATE_ID(Stingby, Notice)
+    DECLARE_STATE_ID(Stingby, Chase)
+    DECLARE_STATE_ID(Stingby, Return)
+
+    DECLARE_STATE_VIRTUAL_ID_OVERRIDE(Stingby, DieOther)
+
+private:
+    JointBlendModel* mModel;
+    EnemyEatData mYoshiEatData;
+    EnemyChibiYoshiEatData mChibiYoshiEatData;
+    const sead::Vector3f mSpawnpoint;
+
+    // Chase state
+    f32 mChaseSpeed;
+    f32 mChaseVelX;
+    f32 mWeavePhase;
+    f32 mWeaveOffset;
+
+    // Bobbing state
+    f32 mBobPhase;
+
+    // Idle (patrol) state
+    f32 mPatrolPhase;
+    f32 mPatrolSpeed;
+
+    // Idle pause quirk
+    s32 mIdlePauseTimer;
+
+    // Aggro puff quirk
+    s32 mAggroPuffTimer;
+};
+
+}
--- src/actor/Stingby.cpp
+++ src/actor/Stingby.cpp
@@ -1,148 +1,229 @@
+#include <zap/actor/Stingby.h>
 #include <zap/Zap.h>
-#include <enemy/Enemy.h>
-#include <enemy/EnemyEatData.h>
-#include <graphics/JointBlendModel.h>
+#include <random/seadGlobalRandom.h>
 #include <graphics/SkeletalAnimation.h>
-#include <telkin/Print.h>
-
-namespace zap {
-
-class Stingby : public Enemy {
-    SEAD_RTTI_OVERRIDE(Stingby, Enemy)
-
-public:
-    static Profile* cProfile;
-
-    Stingby(const ActorCreateParam& param);
-    ~Stingby() override = default;
-
-    Result create() override;
-    bool execute() override;
-    bool draw() override;
-
-    bool createIceActor() override;
-    void setIceAnm() override;
-    
-    void updateModel();
-
-    static inline ActorCollisionCheck::CollisionData cCollisionData = {
-        .center_offset = { 0.0f, 0.0f },
-        .half_size = { 8.5f, 8.5f },
-        .shape_type = ActorCollisionCheck::cShapeType_Box,
-        .kind = ActorCollisionCheck::cKind_Enemy,
-        .attack = ActorCollisionCheck::cAttack_None,
-        .vs_kind = ActorCollisionCheck::TargetKind(
-            ActorCollisionCheck::cTargetKind_Player |
-            ActorCollisionCheck::cTargetKind_Enemy |
-            ActorCollisionCheck::cTargetKind_Item |
-            ActorCollisionCheck::cTargetKind_Tama |
-            ActorCollisionCheck::cTargetKind_ChibiYoshi |
-            ActorCollisionCheck::cTargetKind_Unk10 |
-            ActorCollisionCheck::cTargetKind_DrcTouch
-        ),
-        .vs_damage = ActorCollisionCheck::cDamageFrom_All,
-        .status = ActorCollisionCheck::cStatus_None,
-        .callback = &Enemy::normal_collcheck
-    };
-
-    void vsPlayerHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
-    void vsYoshiHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
-
-    DECLARE_STATE_ID(Stingby, Idle)
-    DECLARE_STATE_ID(Stingby, Notice)
-    DECLARE_STATE_ID(Stingby, Chase)
-
-    DECLARE_STATE_VIRTUAL_ID_OVERRIDE(Stingby, DieOther)
-
-    JointBlendModel* mModel;
-    sead::Vector3f mIdleCenter;
-    EnemyEatData mEatData;
-};
-
-}
 
 SEAD_RTTI_OVERRIDE_IMPL(zap::Stingby, Enemy)
 
 CREATE_STATE_ID(zap::Stingby, Idle)
 CREATE_STATE_ID(zap::Stingby, Notice)
 CREATE_STATE_ID(zap::Stingby, Chase)
+CREATE_STATE_ID(zap::Stingby, Return)
+
 CREATE_STATE_VIRTUAL_ID_OVERRIDE(zap::Stingby, Enemy, DieOther)
 
+// Configuration
+//  Patrol
+static constexpr f32 cPatrolRange           = 4 * 16.0f; // tiles
+static constexpr f32 cNoticeRange           = 4 * 16.0f; // tiles
+static constexpr f32 cForgetRange           = 8 * 16.0f; // tiles
+static constexpr f32 cPatrolCycleFrames     = 560.0f;    // 9.33 seconds
+//  Visual
+static constexpr f32 cScaleFactor           = 0.17f;     // 3DW models are large
+static constexpr f32 cInvScaleFactor        = 1.0f / cScaleFactor;
+static constexpr f32 cAnimBlendTime         = 10.0f;
+static constexpr f32 cAggroPuffCycleFrames  = 30.0f;     // 0.50 seconds
+static constexpr f32 cTurnRate              = 3.0f;      // degrees/frame
+//  Chase
+static constexpr f32 cChaseBaseSpeed        = 0.5f;
+static constexpr f32 cChaseMaxSpeed         = 2.0f;
+static constexpr f32 cChaseAccelFrames      = 156.0f;    // 2.6 seconds
+static constexpr f32 cChaseInertia          = 0.04f;
+// Bob/Weave
+static constexpr f32 cBobCycleFrames        = 131.0f;              // 2.18 seconds
+static constexpr f32 cWeaveCycleFrames      = cBobCycleFrames / 3; // 0.72 seconds
+static constexpr f32 cBobAmplitude          = 2.4f;
+static constexpr f32 cWeaveAmplitude        = 1.6f;
+//  Internal (don't touch these)
+static constexpr f32 cPatrolSpeed           = sead::Mathf::pi2() / cPatrolCycleFrames;
+static constexpr f32 cChaseAccel            = (cChaseMaxSpeed - cChaseBaseSpeed) / cChaseAccelFrames;
+static constexpr f32 cBobPhaseStep          = sead::Mathf::pi2() / cBobCycleFrames;
+static constexpr f32 cWeavePhaseStep        = sead::Mathf::pi2() / cWeaveCycleFrames;
+
+// Register it
 Profile* zap::Stingby::cProfile = zap::getRegistrar()->newProfile<zap::Stingby>("stingby")
     .resources<"hacchin000">(ProfileInfo::cResType_Course)
     .build();
 
+// Hitbox data
+using CC = ActorCollisionCheck;
+ActorCollisionCheck::CollisionData zap::Stingby::cCollisionData = {
+    .center_offset = { 0.0f, 0.0f },
+    .half_size = { 8.5f, 8.5f },
+    .shape_type = CC::cShapeType_Box,
+    .kind = CC::cKind_Enemy,
+    .attack = CC::cAttack_None,
+    .vs_kind = CC::TargetKind( // Copied from Kuribo
+        CC::cTargetKind_Player |
+        CC::cTargetKind_Enemy |
+        CC::cTargetKind_Item |
+        CC::cTargetKind_Tama |
+        CC::cTargetKind_ChibiYoshi |
+        CC::cTargetKind_Unk10 |
+        CC::cTargetKind_DrcTouch
+    ),
+    .vs_damage = CC::cDamageFrom_All,
+    .status = CC::cStatus_None,
+    .callback = &Enemy::normal_collcheck
+};
+
+// Main code
 zap::Stingby::Stingby(const ActorCreateParam& param)
     : Enemy(param)
-    , mIdleCenter(mPos)
-    , mEatData(mActorUniqueID)
+    , mModel(nullptr)
+    , mYoshiEatData(mActorUniqueID)
+    , mChibiYoshiEatData(mActorUniqueID)
+    , mSpawnpoint(mPos)
+    , mChaseSpeed(cChaseBaseSpeed)
+    , mChaseVelX(0.0f)
+    , mWeavePhase(0.0f)
+    , mWeaveOffset(0.0f)
+    , mBobPhase(0.0f)
+    , mPatrolPhase(0.0f)
+    , mPatrolSpeed(cPatrolSpeed)
+    , mIdlePauseTimer(0)
+    , mAggroPuffTimer(0)
 { }
 
 ActorBase::Result zap::Stingby::create() {
-    mModel = JointBlendModel::create("hacchin000", "hacchin000", 2, 0, 0, 0, 0);
+    mDirection = DirType::cDirType_Right; // TODO: Configurable or check player
     
-    mDirection = DirType::cDirType_Right;
-    mScale = sead::Vector3f(0.17f, 0.17f, 0.17f);
+    // Load our model
+    mModel = JointBlendModel::create("hacchin000", "hacchin000", 4);
+    mScale = sead::Vector3f(cScaleFactor, cScaleFactor, cScaleFactor);
     
+    // Hitbox
     mCollisionCheck.set(this, cCollisionData);
     reviveCollisionCheck();
     
-    mEatDataPtr = &mEatData;
-    mEatData.setEatType(EatData::cEatType_Drink);
+    // Terrain collision
+    static const ActorBgCollisionCheck::Sensor foot = { -4.25f, 4.25f,  0.0f };
+    static const ActorBgCollisionCheck::Sensor head = {  0.0f, 0.0f, 8.5f };
+    static const ActorBgCollisionCheck::Sensor wall = {  5.0f, 8.0f,  6.0f };
+    mBgCheckObj.set(this, &foot, &head, &wall);
     
+    // Yoshi eat ability
+    mEatDataPtr = &mYoshiEatData;
+    mYoshiEatData.setEatType(EatData::cEatType_Drink);
+    
+    // Baby Yoshi eat ability
+    mChibiYoshiEatDataPtr = &mChibiYoshiEatData;
+    mChibiYoshiEatData.setEatType(ChibiYoshiEatData::cEatType_Drink);
+
+    // Seed the phases randomly
+    mBobPhase = sead::GlobalRandom::instance()->getF32() * sead::Mathf::pi2();
+    mPatrolPhase = sead::GlobalRandom::instance()->getF32() * sead::Mathf::pi2();
+    mWeavePhase = sead::GlobalRandom::instance()->getF32() * sead::Mathf::pi2();
+
+    // Let's go!
     changeState(Stingby::StateID_Idle);
 
     updateModel();
-
     return cResult_Success;
 }
 
 bool zap::Stingby::execute() {
     executeState();
+    
+    if (isState(StateID_Idle) || isState(StateID_Notice) || isState(StateID_Chase) || isState(StateID_Return)) {
+        // Turn towards the target direction
+        mAngle.y().chaseRest(cBaseAngleY[mDirection], sead::Mathf::deg2idx(cTurnRate));
 
-    if (isState(StateID_Idle) || isState(StateID_Notice) || isState(StateID_Chase)) {
-        mAngle.y().chaseRest(cBaseAngleY[mDirection], 0x2000000);
+        // Chaotic movement
+        mBobPhase += cBobPhaseStep;
+        if (mBobPhase > sead::Mathf::pi2())
+            mBobPhase -= sead::Mathf::pi2();
+        
+        mWeavePhase += cWeavePhaseStep;
+        if (mWeavePhase > sead::Mathf::pi2())
+            mWeavePhase -= sead::Mathf::pi2();
+
+        // Smoothly dampen weave
+        const f32 weaveTarget = isState(StateID_Chase) ? sead::Mathf::sin(mWeavePhase) * cWeaveAmplitude : 0.0f;
+        mWeaveOffset += (weaveTarget - mWeaveOffset) * 0.15f;
+
+        // Apply bob and weave together
+        if (!isState(StateID_Ice)) {
+            mPos.y = mSpawnpoint.y + sead::Mathf::sin(mBobPhase) * cBobAmplitude + mWeaveOffset;
+        }
+
+        // Aggro puff
+        if (mAggroPuffTimer > 0) {
+            const f32 t = static_cast<f32>(mAggroPuffTimer) / cAggroPuffCycleFrames;
+            const f32 puff = sead::Mathf::sin(t * sead::Mathf::pi()) * 0.04f; // peaks at +0.04
+            mScale = sead::Vector3f(cScaleFactor + puff, cScaleFactor + puff, cScaleFactor + puff);
+            mAggroPuffTimer--;
+        }
+        
+        // Baseline reset on the final frame
+        if (mAggroPuffTimer == 0) {
+            mScale = sead::Vector3f(cScaleFactor, cScaleFactor, cScaleFactor);
+        }
     }
 
     updateModel();
-
+    
+    // Accurately track the model with the hitbox via bone
+    Model* model = mModel->getModel();
+    
+    sead::Matrix34f mtx;
+    model->getBoneWorldMatrix(model->searchBoneIndex("JointRoot"), &mtx);
+    
+    sead::Vector3f hitboxPos = mtx.getTranslation() - mPos;
+    mCollisionCheck.setCenterOffsetX(hitboxPos.x);
+    mCollisionCheck.setCenterOffsetY(hitboxPos.y);
+    
+    // TODO: Play a buzzing sound here
+    
+    // TODO: Emit some honey particles or something, maybe
+    
     return true;
 }
 
 bool zap::Stingby::createIceActor() {
-    sead::Vector3f pos(
+    sead::Vector3f pos = {
         mPos.x,
-        mPos.y - 16.0f,
+        mPos.y - 14.0f,
         mPos.z
-    );
-    IceInfo info = { 0, pos, sead::Vector3f(1.5f, 1.5f, 1.5f), nullptr };
+    };
+    
+    IceInfo info = { 
+        IceInfo::makeParam(cIceType_Square),
+        pos,
+        mScale * cInvScaleFactor * 1.5f,
+        nullptr
+    };
     return mIceMgr.createIce(info);
 }
 
 void zap::Stingby::setIceAnm() {
+    // In the notice frame, sometimes the anim can move the model too far, resulting in clipping outside the ice
     if (isState(StateID_Notice)) {
         mModel->setAnm("fly_idle", 0.0f);
+        
+        // Random frame, since we're manually setting the anim we don't want all to look the same
+        f32 maxFrame = mModel->getCurSklAnim()->getFrameCtrl().getFrameMax();
+        mModel->getCurSklAnim()->getFrameCtrl().setFrame(
+            sead::GlobalRandom::instance()->getF32() * maxFrame
+        );
     }
-    // TODO: Random frame: mModel->getCurSklAnim()->getFrameCtrl().setFrame(0.0f);
 }
 
 void zap::Stingby::updateModel() {
     sead::Matrix34f mtx;
     mtx.makeRTIdx(mAngle, mPos);
+    mModel->setMtxRT(mtx);
 
-    mModel->getModel()->setMtxRT(mtx);
-
-    if (!isState(StateID_Ice)) {
+    if (!isState(StateID_Ice)) { // Don't animate if we're frozen, obviously
         mModel->playAnmFrameCtrl();
     }
 
-    mModel->getModel()->setScale(mScale);
+    mModel->setScale(mScale);
     mModel->calcMdl();
 }
 
 bool zap::Stingby::draw() {
     mModel->draw();
-
     return true;
 }
 
@@ -176,39 +257,70 @@ void zap::Stingby::vsYoshiHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCol
             return setDeathInfo_YoshiFumi(other); // changes state to DieYoshiFumi
         }
         
-        case cFumiType_Hit: {
-            return Enemy::vsPlayerHitCheck_Normal(cc_self, cc_other);
+        default: {
+            return Enemy::vsYoshiHitCheck_Normal(cc_self, cc_other);
         }
     }
 }
 
 /** STATE: Idle */
 
-void zap::Stingby::initializeState_Idle() {
-    mIdleCenter = mPos;
-    mDirection = cDirType_Right;
-    mModel->setAnm("fly_idle", 10.0f, FrameCtrl::cMode_Repeat);
+void zap::Stingby::initializeState_Idle() {    
+    // Re-derive the correct patrol phase
+    f32 normalizedX = (mPos.x - mSpawnpoint.x) / cPatrolRange;
+    normalizedX = sead::Mathf::clamp2(normalizedX, -1.0f, 1.0f);
+    mPatrolPhase = sead::Mathf::asin(normalizedX);
+    if (mChaseVelX < 0.0f) {
+        // Mirror if we're moving left
+        mPatrolPhase = sead::Mathf::pi() - mPatrolPhase;
+    }
+
+    // Avoid 1-frame desync (execute updates this)
+    mPos.x = mSpawnpoint.x + sead::Mathf::sin(mPatrolPhase) * cPatrolRange;
+
+    mIdlePauseTimer = 0;
+    mPatrolSpeed = cPatrolSpeed;
+    
+    mModel->setAnm("fly_idle", cAnimBlendTime, FrameCtrl::cMode_Repeat);
 }
 
 void zap::Stingby::executeState_Idle() {
     sead::Vector2f player;
-    searchNearPlayer(player);
-    
-    if (sead::Mathf::abs(player.x) < 5.5f * 16) {
+    if (searchNearPlayer(player) == -1)
+        return; // No player found
+
+    // Notice trigger
+    if (sead::Mathf::abs(player.x) < cNoticeRange) {
         changeState(StateID_Notice);
     }
     
-    if (mDirection == cDirType_Right) {
-        mPos.x += 0.5f;
-        if (mPos.x > mIdleCenter.x + 4 * 16) {
-            mDirection = cDirType_Left;
-        }
+    // Idle pause (0.5% chance per frame)
+    // Smoothly lerp patrol speed for inertia
+    const f32 lerpFactor = 0.18f;
+    if (mIdlePauseTimer > 0) {
+        mIdlePauseTimer--;
+        mPatrolSpeed += (0.0f - mPatrolSpeed) * lerpFactor;
     } else {
-        mPos.x -= 0.5f;
-        if (mPos.x < mIdleCenter.x - 4 * 16) {
-            mDirection = cDirType_Right;
+        if (sead::GlobalRandom::instance()->getU32() % 200 == 0) {
+            mIdlePauseTimer = 90;
         }
+        mPatrolSpeed += (cPatrolSpeed - mPatrolSpeed) * lerpFactor;
     }
+
+    mPatrolPhase += mPatrolSpeed;
+    if (mPatrolPhase > sead::Mathf::pi2()) {
+        mPatrolPhase -= sead::Mathf::pi2();
+    }
+    
+    // Check for terrain collision and turn
+    bgCheck_();
+    if (mBgCheckObj.checkWall(mDirection))
+        mPatrolPhase += sead::Mathf::pi2();
+    
+    mPos.x = mSpawnpoint.x + sead::Mathf::sin(mPatrolPhase) * cPatrolRange;
+
+    // Derive direction from the cosine (derivative of sine) so the model flips smoothly as it changes direction
+    mDirection = sead::Mathf::cos(mPatrolPhase) >= 0 ? cDirType_Right : cDirType_Left;
 }
 
 void zap::Stingby::finalizeState_Idle() { }
@@ -216,10 +328,13 @@ void zap::Stingby::finalizeState_Idle() { }
 /** STATE: Notice */
 
 void zap::Stingby::initializeState_Notice() {
-    mModel->setAnm("notice", 3.0f, FrameCtrl::cMode_NoRepeat);
+    mModel->setAnm("notice", cAnimBlendTime / 3, FrameCtrl::cMode_NoRepeat);
+    
+    // TODO: Play a notice sound effect here
 }
 
 void zap::Stingby::executeState_Notice() {
+    // Wait for the little jump to finish before chasing
     if (mModel->getCurSklAnim()->getFrameCtrl().isStop()) {
         changeState(StateID_Chase);
     }
@@ -230,24 +345,91 @@ void zap::Stingby::finalizeState_Notice() { }
 /** STATE: Chase */
 
 void zap::Stingby::initializeState_Chase() {
-    mModel->setAnm("fly_dash", 10.0f, FrameCtrl::cMode_Repeat);
+    // Reset
+    mChaseSpeed = cChaseBaseSpeed;
+    mChaseVelX = 0.0f;
+
+    // Trigger aggro puff
+    mAggroPuffTimer = cAggroPuffCycleFrames;
+
+    mModel->setAnm("fly_dash", cAnimBlendTime, FrameCtrl::cMode_Repeat);
 }
 
 void zap::Stingby::executeState_Chase() {
+    // Check for terrain collision
+    bgCheck_();
+    
+    // give up and turn if we hit a wall
+    const bool hitWall = mBgCheckObj.checkWall(mDirection);
+    
     sead::Vector2f player;
-    if (searchNearPlayer(player) == -1) {
+    if (searchNearPlayer(player) == -1 || sead::Mathf::abs(player.x) > cForgetRange || hitWall) {
+        // Are we inside the patrol bound?
+        const f32 leftBound = mSpawnpoint.x - cPatrolRange;
+        const f32 rightBound = mSpawnpoint.x + cPatrolRange;
+        if (mPos.x >= leftBound && mPos.x <= rightBound) {
+            // Yes, chill
+            changeState(StateID_Idle);
+        } else {
+            // No, fly back there
+            changeState(StateID_Return);
+        }
+        
+        if (hitWall) {
+            mDirection = InvDirX(mDirection);
+            mChaseSpeed = 0.0f;
+            mChaseVelX = 0.0f;
+        }
+        
         return;
     }
-
-    if (sead::Mathf::abs(player.x) > 6 * 16) {
-        changeState(StateID_Idle);
-    }
     
-    mPos.x += player.x > 0 ? 0.5f : -0.5f;
-    mDirection = player.x > 0 ? cDirType_Right : cDirType_Left;
+    // Accelerate
+    mChaseSpeed = sead::Mathf::min(mChaseSpeed + cChaseAccel, cChaseMaxSpeed);
+
+    // x-inertia
+    f32 targetVelX = player.x > 0 ? mChaseSpeed : -mChaseSpeed;
+    mChaseVelX += (targetVelX - mChaseVelX) * cChaseInertia;
+    mPos.x += mChaseVelX;
+    
+    mDirection = mChaseVelX > 0 ? cDirType_Right : cDirType_Left;
 }
 
 void zap::Stingby::finalizeState_Chase() { }
+
+/** STATE: Return */
+
+void zap::Stingby::initializeState_Return() {
+    mModel->setAnm("fly_idle", cAnimBlendTime, FrameCtrl::cMode_Repeat);
+}
+
+void zap::Stingby::executeState_Return() {
+    // Check for terrain collision, if there's something in the way here then just keep trying idk
+    bgCheck_();
+    
+    const f32 leftBound = mSpawnpoint.x - cPatrolRange;
+    const f32 rightBound = mSpawnpoint.x + cPatrolRange;
+
+    // If we reached the patrol zone, go back to Idle
+    if (mPos.x >= leftBound && mPos.x <= rightBound) {
+        changeState(StateID_Idle);
+        return;
+    }
+
+    // Determine which boundary is closest to fly towards
+    const f32 targetX = (mPos.x < leftBound) ? leftBound : rightBound;
+
+    const f32 returnSpeed = 1.0f; 
+    const f32 targetVelX = (mPos.x < targetX) ? returnSpeed : -returnSpeed;
+
+    // x-inertia
+    mChaseVelX += (targetVelX - mChaseVelX) * cChaseInertia;
+    mPos.x += mChaseVelX;
+    
+    mDirection = mChaseVelX > 0 ? cDirType_Right : cDirType_Left;
+}
+
+void zap::Stingby::finalizeState_Return() { }
 
 /** STATE: DieOther */
 
@@ -257,8 +439,10 @@ void zap::Stingby::initializeState_DieOther() {
 }
 
 void zap::Stingby::executeState_DieOther() {
+    // Wait for squish to finish before deleting
     if (mModel->getCurSklAnim()->getFrameCtrl().isStop()) {
         deleteRequest();
+        removeCollisionCheck();
     }
 }
 
