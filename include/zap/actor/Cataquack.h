27669172427acb7d6bf0b769732331e229b8f934
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2026-05-27 08:57:49 -0300

    actor: Cataquack v2

--- include/zap/actor/Cataquack.h
+++ include/zap/actor/Cataquack.h
@@ -23,9 +23,20 @@ public:
 
     void calcMdl_Base() override;
 
+    void vsPlayerHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    void vsYoshiHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    void vsEnemyHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+
+    bool hitCallback_Fire(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    bool hitCallback_Ice(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    bool hitCallback_Shell(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    
     void handleMovement();
     void setChaseMode(bool active);
-    void controlLaunchHeight();
+    // From KuriboBase
+    void landonEffect();
+    // From KuriboBase
+    bool checkGround();
     
     static const ActorCreateInfo cCreateInfo;
     static const ActorCollisionCheck::CollisionData cCollisionData; // collision info
@@ -33,15 +44,19 @@ public:
     DECLARE_STATE_ID(Cataquack, Walk);
     DECLARE_STATE_ID(Cataquack, Turn);
     DECLARE_STATE_ID(Cataquack, Launch);
+    DECLARE_STATE_VIRTUAL_ID_OVERRIDE(Cataquack, DieFall)
 
 private:
-    //ActorBoxBgCollision mCollider;
     JointBlendModel* mModel;
 
+    // Debug only
+    char mImGuiWindowID[24];
+
+    bool mHasLanded; // From KuriboBase
+    bool mForceLanded; // From KuriboBase
     bool mChasing;
-    ActorUniqueID mTarget; // stores the PlayerBase actor being chased
-    f32 mTargetInitialY;
-    f32 mLaunchHeight;
+    ActorUniqueID mTarget; // Stores the PlayerBase actor being chased
+    sead::Vector2f mLaunchForce; // In NSMBU speed vector units, exponential
 };
 
 } // namespace zap
--- src/actor/Cataquack.cpp
+++ src/actor/Cataquack.cpp
@@ -1,60 +1,35 @@
 #include <actor/ActorMgr.h>
 #include <actor/Profile.h>
+#include <player/Yoshi.h>
 #include <player/PlayerBase.h>
-#include <telkin/Print.h>
+#include <red/util/SpriteUtil.h>
+#include <effect/EffectCreateUtil.h>
 #include <zap/Zap.h>
 #include <zap/actor/Cataquack.h>
+#include <telkin/Print.h>
+#include <imgui/imgui.h>
 
 SEAD_RTTI_OVERRIDE_IMPL(zap::Cataquack, Enemy)
+
+// TODO: "Notice" state like Stingby
 
 CREATE_STATE_ID(zap::Cataquack, Walk)
 CREATE_STATE_ID(zap::Cataquack, Turn)
 CREATE_STATE_ID(zap::Cataquack, Launch)
-
-using CC = ActorCollisionCheck;
-const CC::CollisionData zap::Cataquack::cCollisionData = {
-    .center_offset = { 0.0f, 14.0f },
-    .half_size = { 11.0f, 14.0f },
-    .shape_type = CC::cShapeType_Box,
-    .kind = CC::cKind_Enemy,
-    .attack = CC::cAttack_None,
-    .vs_kind = CC::TargetKind(CC::cTargetKind_Player | CC::cTargetKind_Yoshi | CC::cTargetKind_ChibiYoshi),
-    .vs_damage = CC::DamageFrom(CC::cDamageFrom_Star),
-    .status = CC::cStatus_None,
-    .callback = [](CC* cc_self, CC* cc_other) {
-        const Actor* other = cc_other->getOwner();
-        const ActorType otherType = other->getActorType();
-        if (otherType != cActorType_Player && otherType != cActorType_Yoshi) {
-            return;
-        }
-        const ActorUniqueID otherID = other->getActorUniqueID();
-        Cataquack* self = static_cast<Cataquack*>(cc_self->getOwner());
-        PlayerBase* player = static_cast<PlayerBase*>(ActorMgr::instance()->getActorPtr(otherID));
-
-        // TODO: Detect between player riding Yoshi vs. lone Yoshi
-        //if (otherType == cActorType_Yoshi && !player->isStatus(PlayerBase::cStatus_RidePlayer)) {
-        //    return;
-        //}
-        self->mTarget = otherID;
-        self->mTargetInitialY = player->getPos().y;
-        self->changeState(Cataquack::StateID_Launch);
-    }
-};
+CREATE_STATE_VIRTUAL_ID_OVERRIDE(zap::Cataquack, Enemy, DieFall)
 
 const ActorCreateInfo zap::Cataquack::cCreateInfo = {
     .offset_x = 8, .offset_y = -8,
     .spawn_range = {
         .offset_x = 8, .offset_y = -8,
-        .half_size_x = 32, .half_size_y = 32
+        .half_size_x = 32, .half_size_y = 32,
     },
-    .cull_range = {
-        .up = 0, .down = 0, .left = 0, .right = 0 },
+    .cull_range = { .up = 0, .down = 0, .left = 0, .right = 0 },
     .flag = 0,
 };
 
 constexpr f32 cScaleFactor = 0.1f;
-constexpr f32 cAnimBlendTime = 10.0f;
-constexpr f32 cDefaultLaunchHeight = 8.0f; // in tiles
+constexpr f32 cAnimBlendTime = 8.0f;
 
 Profile* zap::Cataquack::sProfile = zap::getRegistrar()->newProfile<zap::Cataquack>("cataquack")
     .resources<"poihana">(ProfileInfo::cResType_Course)
@@ -65,17 +40,34 @@ Profile* zap::Cataquack::sProfile = zap::getRegistrar()->newProfile<zap::Cataqua
 zap::Cataquack::Cataquack(const ActorCreateParam& param)
     : Enemy(param)
     , mModel(nullptr)
+    , mHasLanded(true)
+    , mForceLanded(false)
     , mChasing(false)
-    , mTargetInitialY(0)
-    , mLaunchHeight(cDefaultLaunchHeight * 16.0f) // TODO: this actually doesnt really work, it gets affected by things that change the player's gravity like minimush
+    , mLaunchForce(0.0f, 4.5f)
 { }
 
 //* ===== Primary Functions ===== *//
 
 ActorBase::Result zap::Cataquack::create() {
+    // anims: walk, throw, overturn
     this->mModel = JointBlendModel::create("poihana", "poihana", 3);
     this->mScale = { cScaleFactor, cScaleFactor, cScaleFactor };
     this->mModel->setAnm("walk", cAnimBlendTime);
+
+    // Launch knockback
+    const u8 customLaunchForceX = red::SpriteUtil::getBitRange(this, 16, 16 + 8); // nybbles 5 & 6
+    if (customLaunchForceX != 0) {
+        this->mLaunchForce.x = customLaunchForceX * 0.1f;
+    }
+    // Launch height
+    const u8 customLaunchForceY = red::SpriteUtil::getBitRange(this, 24, 24 + 8); // nybbles 7 & 8
+    if (customLaunchForceY != 0) {
+        this->mLaunchForce.y = customLaunchForceY * 0.1f;
+    }
+
+    // direction
+    this->mDirection = this->getPlayerDirLR();
+    this->mAngle.y() = cBaseAngleY[this->mDirection];
 
     // sensors
     static const ActorBgCollisionCheck::Sensor foot = { -6.0f, 6.0f, 0.0f };
@@ -83,23 +75,16 @@ ActorBase::Result zap::Cataquack::create() {
     static const ActorBgCollisionCheck::Sensor wall = { 8.0f, 24.0f, 12.0f };
     this->mBgCheckObj.set(this, &foot, &head, &wall);
 
-    this->mDirection = this->getPlayerDirLR();
-    this->mAngle.y() = cBaseAngleY[this->mDirection];
-
+    // collision
     this->mCollisionCheck.set(this, Cataquack::cCollisionData);
     this->reviveCollisionCheck();
 
-    // TODO: investigate if Cloud collider type actually does anything useful
-    /*this->mCollider.set(this, {
-        .pos_offset = { 0.0f, 14.0f },
-        .rot_pivot_offset = { 0.0f, 0.0f },
-        .left_top_offset = { -5.5f, 10.0f },
-        .right_under_offset = { 5.5f, -12.0f },
-        .angle = 0,
-    });
-    this->mCollider.setType(ActorBoxBgCollision::Type::cType_Cloud);
-    ColliderMgr->add(this->mCollider);
-    this->mCollider.execute(); // call in execute */
+    this->mWaterCalcType = cWaterCalcType_EnablePreCheck;
+    this->mCheckWaterNeeded = true; // Required for checkWater to work + makes the actor have water physics in water (less gravity + slower movement)
+
+#ifdef ZAP_DEBUG
+    __os_snprintf(this->mImGuiWindowID, sizeof(this->mImGuiWindowID), "Cataquack %i", this->mActorUniqueID.getValue());
+#endif
 
     this->changeState(Cataquack::StateID_Walk);
     this->calcMdl_Normal();
@@ -107,11 +92,23 @@ ActorBase::Result zap::Cataquack::create() {
 }
 
 bool zap::Cataquack::execute() {
-    this->controlLaunchHeight();
+#ifdef ZAP_DEBUG
+    if (ImGui::Begin(this->mImGuiWindowID)) {
+        ImGui::DragFloat("Launch Y Speed", &this->mLaunchForce.y, 0.2f, 0.0f, 25.6f);
+        ImGui::DragFloat("Launch X Speed", &this->mLaunchForce.x, 0.2f, 0.0f, 25.6f);
+    } ImGui::End();
+#endif
 
     this->executeState();
+    this->landonEffect();
     this->calcMdl_Normal();
     this->screenOutCheck(0);
+
+    // TODO: proper drowning animation
+    if (this->mIsSubmerged) {
+        this->changeState(Cataquack::StateID_DieYoshiFumi);
+    }
+
     return true;
 }
 
@@ -128,6 +125,76 @@ void zap::Cataquack::calcMdl_Base() {
     );
 }
 
+//* ===== Collision ===== *//
+
+// TODO:
+// block barrels (break them)
+// dont die to goalpole
+// maybe do something about mini players holding barrel/POW not being reachable? (causes stalemate)
+
+using CC = ActorCollisionCheck;
+const CC::CollisionData zap::Cataquack::cCollisionData = {
+    .center_offset = { 0.0f, 14.0f },
+    .half_size = { 11.0f, 14.0f },
+    .shape_type = CC::cShapeType_Box,
+    .kind = CC::cKind_Enemy,
+    .attack = CC::cAttack_Shell,
+    .vs_kind = CC::TargetKind(
+        CC::cTargetKind_Player |
+        CC::cTargetKind_Yoshi |
+        CC::cTargetKind_ChibiYoshi |
+        CC::cTargetKind_Enemy |
+        CC::cTargetKind_Tama // Tama = Projectiles (Fireball/Iceball)
+    ),
+    .vs_damage = CC::DamageFrom(CC::cDamageFrom_Star | CC::cDamageFrom_FireBall | CC::cDamageFrom_IceBall),
+    .status = CC::cStatus_None,
+    .callback = &Enemy::normal_collcheck,
+};
+
+void zap::Cataquack::vsPlayerHitCheck_Normal(CC* cc_self, CC* cc_other) {
+    this->mTarget = cc_other->getOwner()->getActorUniqueID();
+    if (!this->isState(Cataquack::StateID_Launch)) {
+        this->changeState(Cataquack::StateID_Launch);
+    }
+}
+
+void zap::Cataquack::vsYoshiHitCheck_Normal(CC* cc_self, CC* cc_other) {
+    const Yoshi* yoshi = cc_other->getOwner<Yoshi>();
+    if (yoshi->getPlayerRideOn() == nullptr) {
+        return; // Don't bounce Yoshis without a rider
+    }
+    
+    this->mTarget = yoshi->getActorUniqueID();
+    if (!this->isState(Cataquack::StateID_Launch)) {
+        this->changeState(Cataquack::StateID_Launch);
+    }
+}
+
+void zap::Cataquack::vsEnemyHitCheck_Normal(CC* cc_self, CC* cc_other) {
+    const f32 enemyHitRevX = cc_self->getRevisionX(ActorCollisionCheck::cKind_Enemy); // stolen from KuriboBase <3
+    
+    // only turn if we hit something in the direction we're facing
+    if (this->mDirection == cDirType_Left && enemyHitRevX > 0.0f || this->mDirection == cDirType_Right && enemyHitRevX < 0.0f) {
+        if (this->isState(Cataquack::StateID_Walk)) {
+            this->changeState(Cataquack::StateID_Turn);
+        }
+    }
+}
+
+bool zap::Cataquack::hitCallback_Fire(CC* cc_self, CC* cc_other) {
+    return false; // immune
+}
+
+bool zap::Cataquack::hitCallback_Ice(CC* cc_self, CC* cc_other) {
+    this->iceballInvalid(cc_other);
+    return false; // immune
+}
+
+// Handles many "throwable" projectiles such as bob-ombs and mechakoopas, not only shells.
+bool zap::Cataquack::hitCallback_Shell(CC* cc_self, CC* cc_other) {
+    return false; // immune
+}
+
 //* ===== Utility Functions ===== *//
 
 void zap::Cataquack::handleMovement() {
@@ -142,24 +209,10 @@ void zap::Cataquack::handleMovement() {
 void zap::Cataquack::setChaseMode(bool active) {
     this->mChasing = active;
 
-    const f32 speedBase = active ? 1.0f : 0.5f;
-    this->mSpeed.x = speedBase * cEnMuki[this->mDirection];
-    this->mSpeedMax.x = speedBase * cEnMuki[this->mDirection];
-    this->mModel->getCurSklAnim()->getFrameCtrl().setRate(speedBase * 2);
-}
-
-void zap::Cataquack::controlLaunchHeight() {
-    if (!this->mTarget.isValid()) {
-        return;
-    }
-    
-    PlayerBase* target = static_cast<PlayerBase*>(ActorMgr::instance()->getActorPtr(this->mTarget));
-    if (target == nullptr || target->getPos().y <= this->mTargetInitialY + this->mLaunchHeight) {
-        return;
-    }
-    // TODO: make this smoother (how does bouncy cloud do it?)
-    target->getSpeedVec().y *= 0.3f;
-    this->mTarget.invalidate();
+    const f32 speed = active ? 1.0f : 0.5f;
+    this->mSpeed.x = speed * cEnMuki[this->mDirection];
+    this->mSpeedMax.x = speed * cEnMuki[this->mDirection];
+    this->mModel->getCurSklAnim()->getFrameCtrl().setRate(speed * 2);
 }
 
 //* ===== State Functions ===== *//
@@ -207,7 +260,7 @@ void zap::Cataquack::initializeState_Turn() {
 void zap::Cataquack::executeState_Turn() {
     this->handleMovement();
 
-    u32 step = sead::Mathf::deg2idx(this->mChasing ? 6.0f : 3.0f);
+    u32 step = sead::Mathf::deg2idx(this->mChasing ? 4.0f : 2.0f);
     if (this->mAngle.y().chaseRest(cBaseAngleY[this->mDirection], step)) {
         this->changeState(Cataquack::StateID_Walk);
     }
@@ -219,16 +272,27 @@ void zap::Cataquack::finalizeState_Turn() {
 
 /** STATE: Launch */
 
-void zap::Cataquack::initializeState_Launch() {
+void zap::Cataquack::initializeState_Launch() { 
     this->mDirection = this->getPlayerDirLR();
     this->mAngle.y() = cBaseAngleY[this->mDirection];
 
-    this->mModel->setAnm("throw", cAnimBlendTime, FrameCtrl::cMode_NoRepeat);
+    this->mModel->setAnm("throw", cAnimBlendTime, FrameCtrl::cMode_NoRepeat, 1.2f);
     this->mSpeed.x = 0;
-    PlayerBase* player = static_cast<PlayerBase*>(ActorMgr::instance()->getActorPtr(this->mTarget));
-    player->getSpeedVec().y = 1.0f + this->mLaunchHeight / 14.0f;
-
-    player->startSound("SE_PLY_RIDE_CLOUD");
+    
+    // Can also be Yoshi
+    if (PlayerBase* playerlike = static_cast<PlayerBase*>(ActorMgr::instance()->getActorPtr(this->mTarget))) {
+        bool bounced = playerlike->bouncePlayer1(
+            this->mLaunchForce.y,
+            this->mLaunchForce.x * cEnMuki[this->mDirection],
+            true,
+            PlayerBase::cBounceType_1,
+            PlayerBase::cJumpSe_None
+        );
+        
+        if (bounced) {
+            GameAudio::getAudioObjMap()->startSound("SE_PLY_RIDE_CLOUD", this->mPos);
+        }
+    }
 }
 
 void zap::Cataquack::executeState_Launch() {
@@ -241,4 +305,118 @@ void zap::Cataquack::executeState_Launch() {
 
 void zap::Cataquack::finalizeState_Launch() {
     this->mModel->setAnm("walk", cAnimBlendTime);
+}
+
+/** STATE: DieFall (Enemy override) */
+// This is the state the enemy is in when spinning out of screen after being defeated by a Star (for example)
+
+void zap::Cataquack::initializeState_DieFall() {
+    Enemy::initializeState_DieFall();
+    this->mHasLanded = false;
+}
+
+void zap::Cataquack::executeState_DieFall() {
+    Enemy::executeState_DieFall();
+    this->landonEffect();
+}
+
+void zap::Cataquack::finalizeState_DieFall() {
+    Enemy::finalizeState_DieFall();
+}
+
+//* ===== KuriboBase Functions ===== *//
+
+void zap::Cataquack::landonEffect() {
+    if (!this->mHasLanded) {
+        sead::Vector3f pos(this->mPos.x, this->mPos.y, 4500.0f);
+
+        if (this->mBgCheckObj.checkFoot()) {
+            pos.z = this->getEffectZPos();
+            this->mHasLanded = true;
+
+            switch (BgUnitCode::getAttr(this->mBgCheckObj.getBgCheckData(cDirType_Down))) {
+                default:
+                    EffectCreateUtil::createEffect(RP_Cmn_LandingSmoke_08, &pos);
+                    break;
+                case BgUnitCode::cNuma:
+                case BgUnitCode::cSand:
+                    EffectCreateUtil::createEffect(RP_Cmn_LandingSand_04, &pos);
+                    break;
+                case BgUnitCode::cIce:
+                    EffectCreateUtil::createEffect(RP_Cmn_LandingIce_04, &pos);
+                    break;
+                case BgUnitCode::cSnow:
+                    EffectCreateUtil::createEffect(RP_Cmn_LandingSnow_04, &pos);
+                    break;
+                case BgUnitCode::cWater:
+                    EffectCreateUtil::createEffect(RP_Cmn_LandingPillarWtr_04, &pos);
+                    break;
+                case BgUnitCode::cNone:
+                    EffectCreateUtil::createEffect(RP_Cmn_LandingSmoke_08, &pos);
+                    break;
+            }
+        } else {
+            sead::Vector3f check_pos = pos;
+            check_pos.y -= 2.0f;
+            WaterType water_type = ActorBgCollisionCheck::checkWater(&pos.y, check_pos, this->mLayer);
+            if (water_type != cWaterType_None) {
+                this->mHasLanded = true;
+                pos.z = 6500.0f;
+
+                switch (water_type) {
+                    default:
+                        break;
+                    case cWaterType_Water:
+                        this->splashEffect_(pos, RP_Cmn_WaterSplash_04, 6, "SE_OBJ_CMN_SPLASH");
+                        break;
+                    case cWaterType_Lava:
+                    case cWaterType_LavaWave:
+                        this->splashEffect_(pos, RP_Cmn_LavaSplash_04, 16, "SE_OBJ_CMN_SPLASH_LAVA");
+                        break;
+                    case cWaterType_Poison:
+                        this->splashEffect_(pos, RP_Cmn_PoisonSplash_04, 23, "SE_OBJ_CMN_SPLASH_POISON");
+                        break;
+                }
+            } else if (this->mForceLanded) {
+                this->mHasLanded = true;
+                sead::Vector3f force_pos(this->mPos.x, this->mPos.y, 4500.0f);
+                EffectCreateUtil::createEffect(RP_Cmn_LandingSmoke_08, &force_pos);
+            }
+        }
+    } else if (!this->mBgCheckObj.checkFoot() && !this->checkGround()) {
+        this->mHasLanded = false;
+
+        sead::Vector3f check_pos = this->mPos;
+        check_pos.y -= 2.0f;
+        WaterType water_type = ActorBgCollisionCheck::checkWater(nullptr, check_pos, this->mLayer);
+
+        if (water_type != cWaterType_None) {
+            this->mHasLanded = true;
+
+            if (this->mForceLanded) {
+                this->mForceLanded = false;
+                this->mHasLanded = false;
+            }
+        } else {
+            if (this->mForceLanded) {
+                this->mHasLanded = true;
+            }
+        }
+    }
+}
+
+bool zap::Cataquack::checkGround() {
+    const BgCollisionCheckParam param = {
+        ._0 = 0,
+        .ignore_quicksand = false,
+        .layer = this->mLayer,
+        .collision_mask = this->mCollisionMask,
+        .type = cBgCollisionCheckType_Solid,
+        .callback = nullptr,
+    };
+    sead::Vector2f p0(this->mPos.x, this->mPos.y);
+    sead::Vector2f p1 = p0;
+    p1.y -= 4.0f;
+    BasicBgCollisionCheck bg_check(param);
+    return bg_check.checkArea(nullptr, p0, p1, 1 << cDirType_Down);
 }
